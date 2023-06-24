6b8735dfc23680a3fe3c386362efba50edfb3825
Joyce <joywg14@gmail.com>
2023-06-24 19:17:07 -0400

    arreglo del agregar eventos

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 app.get ("/eventos", async (req, res) => {
     try {
-        const allEventos = await pool.query("SELECT id_calen_eve, nombre, to_char(fecha_evento :: DATE, 'dd/mm/yyyy') fecha_evento, hora_inicio, descripcion FROM jml_calendario_eventos");
+        const allEventos = await pool.query("SELECT id_calen_eve, nombre, EXTRACT(YEAR FROM ano) ano, to_char(fecha_evento :: DATE, 'dd/mm/yyyy') fecha_evento, hora_inicio, descripcion FROM jml_calendario_eventos");
         res.json(allEventos.rows);
     } catch (error) { 
         console.log(error.message);
@@ -62,10 +62,10 @@ app.get ("/eventosdesfiles", async (req, res) => {
 
 app.post ("/agregarevento", async (req, res) => {
   try {
-        const { ano, nombre, fecha_evento, hora_inicio, tipo_evento, tipo_audiencia, gratis_pago, descripcion } = req.body;
+        const { ano, id_lugar_eventog, nombre, fecha_evento, hora_inicio, tipo_evento, tipo_audiencia, gratis_pago, descripcion } = req.body;
         const agregarEvento = await pool.query(
-            "INSERT INTO JML_calendario_eventos (id_calen_eve, ano, id_lugar_eventog, nombre, fecha_evento, hora_inicio, tipo_evento, tipo_audiencia, gratis_pago, descripcion) VALUES(nextval('JML_id_calen_eve'), $1, 14, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
-            [ano, nombre, fecha_evento, hora_inicio, tipo_evento, tipo_audiencia, gratis_pago, descripcion]);
+            "INSERT INTO JML_calendario_eventos (id_calen_eve, ano, id_lugar_eventog, nombre, fecha_evento, hora_inicio, tipo_evento, tipo_audiencia, gratis_pago, descripcion) VALUES(nextval('JML_id_calen_eve'), $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+            [ano, id_lugar_eventog, nombre, fecha_evento, hora_inicio, tipo_evento, tipo_audiencia, gratis_pago, descripcion]);
         res.json(agregarEvento.rows[0]);
   } catch (error) {
       console.log(error.message);
