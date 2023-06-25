bc021a6f1b12df814f55e713bbac8b977d660d9c
Joyce <joywg14@gmail.com>
2023-06-25 19:57:01 -0400

    listo

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "Mgalvis288",
+    password: "joyce123",
     host: "localhost",
     port: 5432,
     database: "ProyectoBDD"
